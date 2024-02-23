6b577f001f4bba3c4d987f80a0b3f8cdc7e73aa6
khaCey Salvador <khacey.salvador@gmail.com>
2024-02-24 00:01:29 +0900

    footer

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer>
+    <div class="footerContainer">
+        <div class="map">
+            <div class="item">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51888.93238787816!2d139.61384450856656!3d35.62628987482753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d5ccfb0eceb%3A0xc358ca9c60f82437!2sGreen%20Square!5e0!3m2!1sen!2sjp!4v1579412020927!5m2!1sen!2sjp" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+            </div>
+        </div>
+
+        <div class="contact">
+            <div class="firstLine">
+                <div class="item">
+                    <p>Address</p><br>
+                    <p>Green Square Cafe<br>
+                        〒201-0012<br>東京都 狛江市 <br>中和泉 1-2-9<br>松本ビル 2F</p><br>
+                </div>
+                <div class="item">
+                    <p>Phone Number</p><br>
+                    <a href="tel:+81-03-5761-8189">03-5761-8189</a><br><br>
+                    <p>Contact Us</p><br>
+                    <a href="mailto:hello@greensquare.jp?subject=Hey there!">hello@greensquare.jp</a>
+                </div>
+            </div>
+
+            <div class="row">
+                <a href='https://www.instagram.com/greensquare.cafe/' target="_blank"><img width="35" height="35" src="images/instagram.webp" alt="Green Square Instagram" title="The Instagram of Green Square" /></a>
+                <a href='https://www.facebook.com/Green-Square-Cafe-107641744113864/' target="_blank"><img width="35" height="35" src="images/facebook.webp" alt="Green Square Facebook" title="The Facebook of Green Square" /></a>
+                <a href='https://www.twitter.com/GreenSquareCafe' target="_blank"><img width="35" height="35" src="images/twitter.webp" alt="Green Square Twitter" title="The Twitter of Green Square" /></a>
+                <a href='https://www.youtube.com/channel/UCEU2j4lm-cBYu_4tKOl-wZg' target="_blank"><img width="35" height="" src="images/youtube.webp" alt="Green Square Youtube" title="The Youtube of Green Square" /></a>
+                <a href='https://liff.line.me/1645278921-kWRPP32q?accountId=814vrjdy&openerPlatform=native&openerKey=talkroom%3Aheader#mst_challenge=DaU8E0RAZc5Xl3up7OI3sFrx4Pea-AomOCsv7VrUpNg' target="_blank"><img width="35" height="35" src="images/line.webp" alt="Green Square Line" title="The Line Account of Green Square" />
+            </div>
+        </div>
+    </div>
+</footer>
